baa517ea7cd1e7072468b5ecec9ce50bf37552b4
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2024-12-11 15:46:18 +0100

    REFACTOR season display to use descriptive names.
    Replaced numeric season values with descriptive names for better readability. Introduced a switch statement to map season numbers to corresponding names, enhancing the user interface.

--- components/hero-card.tsx
+++ components/hero-card.tsx
@@ -12,11 +12,27 @@ const HeroCard: React.FC<HeroCardModel> = ({
 }) => {
   const imageName = name.replace(/ /g, "-");
 
+  let seasonName;
+
+  switch (season) {
+    case 1:
+      seasonName = "One";
+      break;
+    case 2:
+      seasonName = "Two";
+      break;
+    case 3:
+      seasonName = "Marvel";
+      break;
+    case 4:
+      seasonName = "Christmas";
+  }
+
   return (
     <Card className="py-4 hover:cursor-pointer hover:scale-105 transition-transform duration-200 ease-in-out flex flex-row md:flex-col h-32 md:h-auto">
       <CardHeader className="pb-0 pt-2 px-4 flex-grow flex-col items-start w-auto md:w-full">
         <h4 className="font-bold text-large">{name}</h4>
-        <small className="text-default-500">Season: {season}</small>
+        <small className="text-default-500">Season: {seasonName}</small>
         <small className="text-default-500">Difficulty: {difficulty}</small>
         <small className="text-default-500">Games: {games}</small>
       </CardHeader>
